2af29263e05f834b50e17049d0d240deefb02578
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-03-12 16:53:10 +0600

    dfd

--- dom.js
+++ dom.js
@@ -182,3 +182,43 @@ var listItem = document.querySelector("#items");
 //   console.log(e.ctrlKey);
 //   console.log(e.shiftKey);
 // }
+// var button = document.getElementById("button");
+// button.addEventListener("click", runEvent);
+// button.addEventListener("dblclick", runEvent);
+// button.addEventListener("mousedown", runEvent);
+// button.addEventListener("mouseup", runEvent);
+// var box = document.getElementById("box");
+// box.addEventListener("mouseenter", runEvent);
+// box.addEventListener("mouseleave", runEvent);
+// box.addEventListener("mouseover", runEvent);
+// box.addEventListener("mouseout", runEvent);
+// box.addEventListener("mousemove", runEvent);
+
+var itemInput = document.querySelector('input[type = "text"]');
+var form = document.querySelector("form");
+var select = document.querySelector("select");
+// itemInput.addEventListener("keydown", runEvent);
+// itemInput.addEventListener("keyup", runEvent);
+// itemInput.addEventListener("keypress", runEvent);
+// itemInput.addEventListener("focus", runEvent);
+// itemInput.addEventListener("blur", runEvent);
+
+// itemInput.addEventListener("cut", runEvent);
+// itemInput.addEventListener("paste", runEvent);
+// itemInput.addEventListener("input", runEvent);
+// select.addEventListener("change", runEvent);
+// select.addEventListener("input", runEvent);
+form.addEventListener("submit", runEvent);
+function runEvent(e) {
+  e.preventDefault();
+  console.log("TYPE IS " + e.type);
+  //   console.log(e.target.value);
+  //   document.getElementById("output").innerHTML =
+  //     "<h3>" + e.target.value + "</h3>";
+  //   output.innerHTML =
+  //     "<h3>MouseX :" + e.offsetX + "</h3><h3>MouseY :" + e.offsetY + "</h3>";
+
+  //   box.style.backgroundColor = "rgb(" + e.offsetX + "," + e.offsetY + ",40)";
+  //   document.body.style.backgroundColor =
+  //     "rgb(" + e.offsetX + "," + e.offsetY + ",40)";
+}
